e9611e246a6326c43f6f07b77c6360584882e7ce
basoariff <100922414+basoariff@users.noreply.github.com>
2022-03-26 11:14:37 +0700

    Add files via upload

---
+++ SayaTubeVideo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SayaTubeVideo
+{
+	internal class SayaTubeVideo
+	{
+		private int id;
+		private string title;
+		public int playCount;
+
+	public SayaTubeVideo(string)
+		{
+			Random random = new Random();
+			id = random.Next(0, 100000);
+			playCount = 0;
+		}
+
+	public void IncreasePlayCount(int count)
+		{
+			playCount = playCount + count;
+		}
+
+	public int GetPlayCount()
+		{
+			this.uploadedVideos =
+		}
+
+	public string GetTitle()
+        {
+			return title;
+        }
+
+	public void PrintAllVideoPlaycount()
+		{
+			Console.WriteLine("ID : " + id);
+			Console.WriteLine("TITLE : " + title);
+			Console.WriteLine("PLAYCOUNT : " + playCount);
+		}
+
+	}
+}
